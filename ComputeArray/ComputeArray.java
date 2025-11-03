0a810b74b873b29da56e5c782dd317719fd57dbc
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-03 17:18:42 +0100

    compute array

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,16 @@
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+        int[] result = new int[array.length];
+
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] % 3 == 0) {
+                result[i] = array[i] * 5;
+            } else if ((array[i] - 1 ) % 3  == 0) {
+                result[i] = array[i] + 7;
+            } else if ((array[i] - 2 ) % 3  == 0) {
+                result[i] = array[i] ;
+            }
+        }
+        return result;
+    }
+}
