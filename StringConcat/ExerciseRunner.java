42e5f841dfb08ee9a9fd158f10ab500e2ff7522c
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-03 11:51:29 +0100

    concat to strings

---
+++ StringConcat/ExerciseRunner.java
@@ -0,0 +1,7 @@
+public class ExerciseRunner {
+    public static void main(String[] args) {
+        System.out.println(StringConcat.concat("Hello ", "étudiant !"));
+        System.out.println(StringConcat.concat("", "Hello World !"));
+        System.out.println(StringConcat.concat("Hello", null));
+    }
+}
