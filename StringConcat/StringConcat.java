e2eda621dfa479b986ffa934aff59eddba1710da
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-03 11:54:50 +0100

    return null if both strings are null

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,10 +1,13 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return null;
+        }
         if (s1 == null) {
-            s1 = "";
+            return s2;
         }
         if (s2 == null) {
-            s2 = "";
+            return s1;
         }
         return s1.concat(s2);
     }
