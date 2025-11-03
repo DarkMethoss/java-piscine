73277f2969ad5f140432e79d49aa75f711bd740f
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-03 17:05:41 +0100

    end of file error, hope it's fixed

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,9 +1,9 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        if (s ==  null) {
+        if (s == null) {
             return false;
         }
         String reversed = new String(new StringBuffer(s).reverse());
-        return s.equalsIgnoreCase(reversed) ;
+        return s.equalsIgnoreCase(reversed);
     }
-}g
+}
