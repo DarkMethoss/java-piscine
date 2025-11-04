b7229c58f2abf876519ad97a29c33c01a4065cff
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-04 12:51:31 +0100

    java args

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -3,7 +3,7 @@ public class DoOp {
         if (args == null || args.length == 0) {
             return "Error";
         }
-        
+
         int operand1 = Integer.parseInt(args[0]);
         String operation = args[1];
         int operand2 = Integer.parseInt(args[2]);
@@ -16,11 +16,15 @@ public class DoOp {
             case "*":
                 return (operand1 * operand2) + "";
             case "/":
-            case "%":
                 if (operand2 == 0) {
                     return "Error";
                 }
                 return (operand1 / operand2) + "";
+            case "%":
+                if (operand2 == 0) {
+                    return "Error";
+                }
+                return (operand1 % operand2) + "";
             default:
                 return "Error";
         }
