352a8c857515d13db374b55ce26005746aa5f0d1
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-04 19:07:41 +0100

    :)

---
+++ Capitalize/Capitalize.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Capitalize {
+    public static void capitalize(String[] args) throws IOException {
+        File input = new File(args[0]);
+        File result = new File(args[1]);
+
+        try (Scanner scanner = new Scanner(input)) {
+            FileWriter writer = new FileWriter(args[1],true);
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                String capitalized = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+                if (scanner.hasNext()) {
+                    capitalized += " ";
+                }
+                writer.write(capitalized);
+            }
+            writer.close();
+        }
+    }
+
+}
