11603065fd1ed67f2a39b3ca06cbdabe07bf15b8
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-04 12:50:10 +0100

    java args

---
+++ DoOp/DoOp.java
@@ -0,0 +1,29 @@
+public class DoOp {
+    public static String operate(String[] args) {
+        if (args == null || args.length == 0) {
+            return "Error";
+        }
+        
+        int operand1 = Integer.parseInt(args[0]);
+        String operation = args[1];
+        int operand2 = Integer.parseInt(args[2]);
+
+        switch (operation) {
+            case "+":
+                return (operand1 + operand2) + "";
+            case "-":
+                return (operand1 - operand2) + "";
+            case "*":
+                return (operand1 * operand2) + "";
+            case "/":
+            case "%":
+                if (operand2 == 0) {
+                    return "Error";
+                }
+                return (operand1 / operand2) + "";
+            default:
+                return "Error";
+        }
+
+    }
+}
