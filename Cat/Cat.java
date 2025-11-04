ce79f164be07fe19b5aaa739d9d053010a9a9fbe
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-04 16:28:59 +0100

    handle out of bound exception

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -2,9 +2,11 @@ import java.io.*;
 
 public class Cat {
     public static void cat(String[] args) throws IOException {
-        if (args == null) {
+        if (args == null || args.length == 0) {
             return;
         }
+
+        
 
         try (FileInputStream fileStream = new FileInputStream(args[0])) {
             System.out.write(fileStream.readAllBytes());
