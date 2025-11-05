e7dbd2313d1ee0dafb0a67c03554872578d6e72d
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-05 14:27:56 +0100

    -_-

--- StarUtils/CelestialObject.java
+++ StarUtils/CelestialObject.java
@@ -71,7 +71,7 @@ public class CelestialObject {
 
     public Boolean equals(CelestialObject object) {
         if (object == null) {
-            return false
+            return false;
         }
         return x == object.x
             && y == object.y
