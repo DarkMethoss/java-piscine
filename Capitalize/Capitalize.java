d70ff64246233ea1792f24bf18d11ad375a96ea0
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-05 09:44:38 +0100

    Madara's

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,22 +1,27 @@
 import java.io.*;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Capitalize {
-    public static void capitalize(String[] args) throws IOException {
-        File input = new File(args[0]);
-
-        Scanner scanner = new Scanner(input) ;
-        FileWriter writer = new FileWriter(args[1],true);
-        while (scanner.hasNext()) {
-            String word = scanner.next();
-            String capitalized = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
-            if (scanner.hasNext()) {
-                capitalized += " ";
-            }
-            writer.write(capitalized);
+      public static void capitalize(String[] args) throws IOException {
+        if (args.length != 2) {
+            return;
         }
-        writer.close();
-        
+        String filename1 = args[0];
+        String filename2 = args[1];
+        Path path1 = Path.of(filename1);
+        Path path2 = Path.of(filename2);
+        String content = Files.readString(path1);
+        String[] words = content.trim().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.length() > 0) {
+                words[i] = Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+            }
+        }
+        String result = String.join(" ", words);
+        Files.writeString(path2, result);
+
     }
 
 }
