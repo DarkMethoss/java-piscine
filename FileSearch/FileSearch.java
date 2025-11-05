b86ba3fee109743b9d7c2e5caf5a1cbddf320b1c
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-05 11:57:59 +0100

    serach completed :)

---
+++ FileSearch/FileSearch.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+public class FileSearch {
+    public static String searchFile(String fileName) {
+        ArrayList<String> directories = new ArrayList<>(List.of("document")); 
+        String filePath = "";
+
+        while (directories.size() != 0 && filePath.isEmpty()) {
+            File dir = new File(directories.remove(0));
+            File[] files = dir.listFiles();
+            if (files == null) {
+                break;
+            }
+            for (File file : files) {
+                if (file.isDirectory()){
+                    directories.add(dir + "/" + file.getName());
+                } else if (file.getName().equals(fileName)) {
+                    filePath = dir + "/" + file.getName();
+                }
+            }
+        }
+        return filePath;
+    }
+
+}
