b6c5bf0d08a6f93f6a6d6e5918b54455859dd10d
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-05 17:01:56 +0100

    start planer discoverd

---
+++ StarPlanet/Planet.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+public class Planet extends CelestialObject{
+    private Star centerStar;
+
+    public Planet() {
+        super();
+        centerStar = new Star();
+    }
+
+    public Planet(String name, double x, double y, double z, Star centerStar) {
+        super(name, x, y, z);
+        this.centerStar = centerStar;
+    }
+
+    public Star getCenterStar() {
+        return this.centerStar;
+    }
+
+    public void setCenterStar(Star centerStar) {
+        this.centerStar = centerStar;
+    }
+
+    public String toString(){
+        double distance = CelestialObject.getDistanceBetween(this, this.centerStar);
+        return String.format("%s circles around %s at the %.3f AU", name,centerStar.getName(), distance);
+    }
+
+    @Override
+    public Boolean equals(CelestialObject object) {
+        if (object == null) {
+            return false;
+        }
+        
+        if (!(object instanceof Planet)) {
+            return false;
+        }
+        
+        Planet castedObject = (Planet) object; 
+        return x == castedObject.x
+            && y == castedObject.y
+            && z == castedObject.z
+            && centerStar.equals(castedObject.getCenterStar())
+            && name.equals(castedObject.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(x, y, z, name, centerStar);
+    }
+}
---
+++ StarPlanet/Star.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+public class Star extends CelestialObject {
+    private double magnitude;
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+    public Star() {
+        super();
+        this.magnitude = 0;
+    }
+
+    public double getMagnitude(){
+        return magnitude;
+    }
+
+    public void setMagnitude(double value){
+        this.magnitude = value;
+    }
+
+    public String toString(){
+        return String.format("%s shines at the %.3f magnitude", name, magnitude);
+    }
+
+    @Override
+    public Boolean equals(CelestialObject object) {
+        // if (this == object) {
+        //     return true;
+        // }
+
+        if (object == null) {
+            return false;
+        }
+        
+        if (!(object instanceof Star)) {
+            return false;
+        }
+        
+        Star castedObject = (Star) object; 
+        return x == castedObject.x
+            && y == castedObject.y
+            && z == castedObject.z
+            && magnitude == castedObject.magnitude
+            && name.equals(castedObject.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(x, y, z, name, magnitude);
+    }
+}
