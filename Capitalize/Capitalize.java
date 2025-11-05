0894601984d228864de9a5613f4e198aa6e758af
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-05 09:43:59 +0100

    ???

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -5,18 +5,18 @@ public class Capitalize {
     public static void capitalize(String[] args) throws IOException {
         File input = new File(args[0]);
 
-        try (Scanner scanner = new Scanner(input)) {
-            FileWriter writer = new FileWriter(args[1],true);
-            while (scanner.hasNext()) {
-                String word = scanner.next();
-                String capitalized = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
-                if (scanner.hasNext()) {
-                    capitalized += " ";
-                }
-                writer.write(capitalized);
+        Scanner scanner = new Scanner(input) ;
+        FileWriter writer = new FileWriter(args[1],true);
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            String capitalized = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+            if (scanner.hasNext()) {
+                capitalized += " ";
             }
-            writer.close();
+            writer.write(capitalized);
         }
+        writer.close();
+        
     }
 
 }
