efc18ab5322596c86ffe5e6f415c9b99ccf63455
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-14 16:53:20 +0100

    create a new modifiable list

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -1,21 +1,26 @@
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Collections;
 
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-        if (list == null) {
+           if (list == null) {
             return null;
         }
-        Collections.sort(list);
-        return list;
+        List<Integer> newList = new ArrayList<>(list);
+
+        Collections.sort(newList);
+        return newList;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
         if (list == null) {
             return null;
         }
-        Collections.sort(list, Collections.reverseOrder());
-        return list;
+        List<Integer> newList = new ArrayList<>(list);
+            
+        Collections.sort(newList, Collections.reverseOrder());
+        return newList;
     }
 }
