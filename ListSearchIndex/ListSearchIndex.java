72287b7155f2cc3b57685a3283817cf9bad9faf3
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-14 14:54:25 +0100

    search list

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListSearchIndex {
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        return list.lastIndexOf(value);
+    }
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        return list.lastIndexOf(value);
+    }
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i< list.size(); i++){
+            Integer nb = list.get(i);
+            if (nb == value){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
