887c028b05859211710d6799fc15e87586a13344
DarkMethoss <ayoubstudies@gmail.com>
2025-11-15 15:31:49 +0100

    start the ceremony, couples are here :)

---
+++ Wedding/Wedding.java
@@ -0,0 +1,30 @@
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class Wedding {
+    public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
+        List<String> list1 = first.size() > second.size() ?  second.stream().toList(): first.stream().toList();
+        List<String> list2 =  second.size() > first.size()  ? first.stream().toList(): second.stream().toList();
+
+        List<String>  shortList = new ArrayList<String>(list1) ;
+        List<String>  longList = new ArrayList<String>(list2) ;
+
+
+        return shortList.stream().collect(Collectors.toMap(
+            s -> s,
+            value -> {
+                Random r = new Random();
+                int index = r.nextInt(longList.size());
+                String v = longList.get(index);
+                longList.remove(index);
+                return v;
+            }
+        ));
+    }
+}
