d3fd91b2f3f9ef62cb8e3154cdd6667e70ae525b
DarkMethoss <ayoubstudies@gmail.com>
2025-11-15 16:09:28 +0100

    .

--- Wedding/Wedding.java
+++ Wedding/Wedding.java
@@ -1,31 +1,29 @@
 import java.util.Set;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class Wedding {
     public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
-        List<String> list1 = first.size() > second.size() ?  second.stream().toList(): first.stream().toList();
-        List<String> list2 =  first.size() > second.size()  ? first.stream().toList(): second.stream().toList();
+        
+        Map<String, String> res = new HashMap<>();
+        List<String>  firstList = new ArrayList<>(first) ; 
+        List<String>  secondList = new ArrayList<>(second) ;
 
-        List<String>  shortList = new ArrayList<String>(list1) ;
-        List<String>  longList = new ArrayList<String>(list2) ;
+        while (firstList.size() != 0 && secondList.size() != 0) {
+            Random r = new Random();
+            int firstCouple = r.nextInt(firstList.size());
+            int secondCouple = r.nextInt(secondList.size());
 
-        // int[] =
+            res.put(firstList.get(firstCouple), secondList.get(secondCouple));
+            firstList.remove(firstCouple);
+            secondList.remove(secondCouple);
+        }
 
-        return shortList.stream().collect(Collectors.toMap(
-            s -> s,
-            s -> {
-                Random r = new Random();
-                int index = r.nextInt(longList.size());
-                String v = longList.get(index);
-                longList.remove(index);
-                return v;
-            }
-        ));
+        return res;
     }
 }
