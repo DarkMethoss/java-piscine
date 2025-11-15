3451ade1757b1a57af7687bfbacc975a6ad4c4c7
DarkMethoss <ayoubstudies@gmail.com>
2025-11-15 15:44:00 +0100

    .

--- Wedding/Wedding.java
+++ Wedding/Wedding.java
@@ -10,15 +10,16 @@ import java.util.Random;
 public class Wedding {
     public static Map<String, String> createCouple(Set<String> first, Set<String> second) {
         List<String> list1 = first.size() > second.size() ?  second.stream().toList(): first.stream().toList();
-        List<String> list2 =  second.size() > first.size()  ? first.stream().toList(): second.stream().toList();
+        List<String> list2 =  first.size() > second.size()  ? first.stream().toList(): second.stream().toList();
 
         List<String>  shortList = new ArrayList<String>(list1) ;
         List<String>  longList = new ArrayList<String>(list2) ;
 
+        // int[] =
 
         return shortList.stream().collect(Collectors.toMap(
             s -> s,
-            value -> {
+            s -> {
                 Random r = new Random();
                 int index = r.nextInt(longList.size());
                 String v = longList.get(index);
