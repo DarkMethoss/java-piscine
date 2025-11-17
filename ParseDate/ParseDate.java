c6c3beda6d93562779b6cbda164562fb19f26c0c
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-17 16:13:04 +0100

    handle npe

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -12,13 +12,18 @@ import java.util.Map;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
-
+        if (stringDate == null) {
+            return null;
+        }
         DateTimeFormatter format = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
         LocalDateTime parsedDate = LocalDateTime.parse(stringDate, format);
         return parsedDate;
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null) {
+            return null;
+        }
         DateTimeFormatter formatter = new DateTimeFormatterBuilder()
                 .parseCaseInsensitive()
                 .appendPattern("EEEE dd MMMM yyyy")
@@ -28,26 +33,23 @@ public class ParseDate {
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
+        if (stringDate == null) {
+            return null;
+        }
         Map<Long, String> ampmMap = Map.of(
                 0L, "in the morning",
                 1L, "in the evening");
 
         DateTimeFormatter formatter = new DateTimeFormatterBuilder()
                 .parseCaseInsensitive()
-
                 .appendValue(ChronoField.HOUR_OF_AMPM, 2)
                 .appendLiteral(" hours ")
-
                 .appendText(ChronoField.AMPM_OF_DAY, ampmMap)
                 .appendLiteral(", ")
-
                 .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
                 .appendLiteral(" minutes and ")
-
-                // seconds
                 .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
                 .appendLiteral(" seconds")
-
                 .toFormatter(Locale.ENGLISH);
 
         return LocalTime.parse(stringDate, formatter);
