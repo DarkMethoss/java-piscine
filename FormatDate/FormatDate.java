f402fee7de16be9eff88030d5224a2480c5447cd
DarkMethoss <ayoubstudies@gmail.com>
2025-11-17 18:29:31 +0100

    the nano seconds are optional

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -13,20 +13,19 @@ public class FormatDate {
             return null;
         }
         DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder()
-            .appendLiteral("Le ")
-            .appendValue(ChronoField.DAY_OF_MONTH, 2)
-            .appendLiteral(" ")
-            .appendText(ChronoField.MONTH_OF_YEAR, TextStyle.SHORT)
-            .appendLiteral(" de l'an ")
-            .appendValue(ChronoField.YEAR,4)
-            .appendLiteral(" à ")
-            .appendValue(ChronoField.HOUR_OF_DAY,2)
-            .appendLiteral("h")
-            .appendValue(ChronoField.MINUTE_OF_HOUR,2)
-            .appendLiteral("m et ")
-            .appendValue(ChronoField.SECOND_OF_MINUTE,2)
-            .appendLiteral("s")
-            ;
+                .appendLiteral("Le ")
+                .appendValue(ChronoField.DAY_OF_MONTH, 2)
+                .appendLiteral(" ")
+                .appendText(ChronoField.MONTH_OF_YEAR, TextStyle.SHORT)
+                .appendLiteral(" de l'an ")
+                .appendValue(ChronoField.YEAR, 4)
+                .appendLiteral(" à ")
+                .appendValue(ChronoField.HOUR_OF_DAY, 2)
+                .appendLiteral("h")
+                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+                .appendLiteral("m et ")
+                .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+                .appendLiteral("s");
         return dateTime.format(builder.toFormatter(Locale.FRENCH));
     }
 
@@ -43,7 +42,12 @@ public class FormatDate {
         if (time == null) {
             return null;
         }
-        DateTimeFormatter f = DateTimeFormatter.ofPattern("HH:mm:ss:n");
+        DateTimeFormatter f = new DateTimeFormatterBuilder()
+                .appendPattern("HH:mm:ss")
+                .optionalStart()
+                .appendFraction(ChronoField.NANO_OF_SECOND, 1, 9, true)
+                .optionalEnd()
+                .toFormatter();
         return time.format(f);
     }
 
