18c7d84bec3323f7f90645a03179c0a456a09c7a
DarkMethoss <ayoubstudies01@gmail.com>
2025-11-18 09:37:13 +0100

    containsOnlyAlpha lower and upercase included

--- RegexMatch/ExerciseRunner.java
+++ RegexMatch/ExerciseRunner.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class ExerciseRunner {
     public static void main(String[] args) throws IOException {
-        System.out.println(RegexMatch.containsOnlyAlpha("azejkdfhjsdf"));// true
+        System.out.println(RegexMatch.containsOnlyAlpha("AEFdkjdf"));// true
         System.out.println(RegexMatch.containsOnlyAlpha("azejkd fhjsdf"));// flase
         System.out.println(RegexMatch.startWithLetterAndEndWithNumber("asjd jd34jds jkfd6")); // true
         System.out.println(RegexMatch.startWithLetterAndEndWithNumber("asjd jd34jds jkfd6.")); // false
--- RegexMatch/RegexMatch.java
+++ RegexMatch/RegexMatch.java
@@ -1,6 +1,6 @@
 public class RegexMatch {
     public static boolean containsOnlyAlpha(String s) {
-        return s.matches("[a-z]*");
+        return s.matches("[a-zA-Z]*");
     }
     
     public static boolean startWithLetterAndEndWithNumber(String s) {
